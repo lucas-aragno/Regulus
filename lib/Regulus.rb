1dc0e51b595c5230fd1f61d5c8d1d676ff82f06c
laragno <lucas.aragno157@gmail.com>
2015-06-17 16:35:49 -0300

    adding zip code and currency matches moving scann process to a class

--- lib/Regulus.rb
+++ lib/Regulus.rb
@@ -2,6 +2,21 @@ require "Regulus/version"
 
 module Regulus
   def self.match_0_to_999999(string)
-     string.scan(/^\d{1,6}$/)
+   s = Scanner.new  
+   s.perform(string,/^\d{1,6}$/)
   end
+
+  def self.is_zip_code?(string)
+   s = Scanner.new 
+   not s.perform(string, /^\d{5}(-\d{4})?$/).empty?
+  end
+
+  def self.is_currency?(string)
+   s = Scanner.new
+   not s.perform(string, /^-?(?:0|[1-9]\d{0,2}(?:,?\d{3})*)(?:\.\d+)?$/).empty?
+  end
+
+  class Scanner
+    def perform(string, regex); string.scan(regex); end
+   end
 end
--- test/Regulus/regulus_test.rb
+++ test/Regulus/regulus_test.rb
@@ -17,4 +17,20 @@ describe Regulus do
   it 'shouldnt match numebers bigger than 999999' do
     Regulus.match_0_to_999999("1000000").must_be_empty
   end
+
+  it 'should match US zip codes' do
+    Regulus.is_zip_code?("94941-3232").must_equal true
+  end
+ 
+  it 'shouldnt match as a US zip code' do
+    Regulus.is_zip_code?("nope").wont_equal true
+  end
+
+  it 'should match US currency' do
+    Regulus.is_currency?("20").must_equal true
+  end
+
+  it 'shouldn match as US currency' do
+    Regulus.is_currency?("$.99").wont_equal true
+  end
 end
